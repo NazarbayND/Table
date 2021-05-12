3bc2cd118c5cfa9f4ae13cdc7900deff6e1e523f
NazarbayND <nazarbay.ktl@gmail.com>
2021-05-12 16:56:23 +0600

    case-sensitivity fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import Records from "./modules/NewTable/Records";
-import TrainingRecordsModule from "./modules/TrainingRecordsModule/TrainingRecordsModule";
 
 function App() {
   return (
     <div className="App">
-      {/* <TrainingRecordsModule /> */}
       <Records />
     </div>
   );
